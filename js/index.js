3f2e45b8dba508593a3a07d9a1f96c39dfa24231
Regina <reginaSirazieva1987@gmail.com>
2023-08-21 15:47:02 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -324,50 +324,28 @@ const json=JSON.stringify(Object.fromEntries(formData.entries()));
 
  // Slider
 
- let slideIndex = 1;
- const slides = document.querySelectorAll('.offer__slide'),
-     prev = document.querySelector('.offer__slider-prev'),
-     next = document.querySelector('.offer__slider-next'),
-     total = document.querySelector('#total'),
-     current = document.querySelector('#current');
+ const slides = document.querySelectorAll(".offer__slide"),
+		prev = document.querySelector(".offer__slider-prev"),
+		next = document.querySelector(".offer__slider-next"),
+		total = document.querySelector("#total"),
+		current = document.querySelector("#current"),
+		slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+		width = window.getComputedStyle(slidesWrapper).width,
+		slidesField = document.querySelector(".offer__slider-inner");
+        let slideIndex = 1;
 
- showSlides(slideIndex);
 
- if (slides.length < 10) {
-     total.textContent = `0${slides.length}`;
- } else {
-     total.textContent = slides.length;
- }
 
- function showSlides(n) {
-     if (n > slides.length) {
-         slideIndex = 1;
-     }
-     if (n < 1) {
-         slideIndex = slides.length;
-     }
 
-     slides.forEach((item) => item.style.display = 'none');
 
-     slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
-     
-     if (slides.length < 10) {
-         current.textContent =  `0${slideIndex}`;
-     } else {
-         current.textContent =  slideIndex;
-     }
- }
-
- function plusSlides (n) {
-     showSlides(slideIndex += n);
- }
-
- prev.addEventListener('click', function(){
-     plusSlides(-1);
- });
-
- next.addEventListener('click', function(){
-     plusSlides(1);
- });
+     slidesField.style.width = 100 * slides.length + "%";
+     slidesField.style.display = "flex";
+     slidesField.style.transition = "0.5s all";
+ 
+ 
+     slides.forEach((slide) => {
+         slide.style.width = width;
+     });
+ 
 
 });
